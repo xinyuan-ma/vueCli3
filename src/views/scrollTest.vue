bc7211e8fd363714e5447fcb5c49e7a5f06bd8b0
BWH\Chris.Ma <chris.ma@bestwehotel.com>
2019-11-13 14:53:55 +0800

    增加网络的判断

--- src/views/scrollTest.vue
+++ src/views/scrollTest.vue
@@ -1,9 +1,9 @@
 /**
-*  该模块测试@scroll 方法，vue自带方法
+*  该模块测试@scroll 方法，vue自带方法; 判断是否有网
 */
 <template>
 	<div class="scrollTest" @scroll="handleScroll">
-		<p v-for="(item, key) in 80" :key="key">{{item}}</p>
+		<p v-for="(item, key) in 10" :key="key">{{item}}</p>
 	</div>
 </template>
 
@@ -19,6 +19,12 @@ export default {
 		}
 	},
 	mounted () {
+		window.addEventListener('online', function () {
+			console.log('网络打开', window.navigator.onLine)
+		})
+		window.addEventListener('offline', function () {
+			console.log('网络关闭', window.navigator.onLine)
+		})
 	},
 	methods: {
 		handleScroll () {
