80c2c429574261c68250c65073f2dcb2b014f537
xinyuan <13837120071@163.com>
2020-09-28 11:29:25 +0800

    增加深拷贝方法

--- src/common/index.js
+++ src/common/index.js
@@ -51,3 +51,51 @@ export const asyncLoadJs = (url, isLoadScript) => {
 		};
 	});
 }
+
+// let obj = {
+//   name: 'yuan',
+//   arr: [1, [2, 3], 4],
+//   fn: function () {
+//     console.log('fn');
+//   },
+//   un: undefined,
+//   nul: null,
+//   reg: /boo/i
+// }
+// obj.test = obj
+
+// yuan.js?0fad:79 Uncaught RangeError: Maximum call stack size exceeded
+// 死循环
+
+// let obj1 = deepClone(obj)
+// obj1.arr = [1]
+// console.log(obj, obj1, '123');
+
+/**
+ * 深拷贝
+ * @param {any} val 要拷贝的数据(最终解决方案，可以拷贝所有的数据，包括自身和原型的)
+ * @param {object} map weakMap用于记录已拷贝的对象，解决对象的属性直接的引用了自身的情况，避免循环调用
+ */
+
+export function deepClone(val, map = new WeakMap()) {
+  // RegExp 和 Date 这两种类型 typeof date 为 ’object‘ 但这两种类型不可便利，即不能使用for in方法，要单独处理
+  if (val instanceof Date) return new Date(val)
+  if (val instanceof RegExp) return new RegExp(val)
+  if (val === null) return null // typeof null 为’object‘，但是null没有constructor属性，直接返回null
+  if (typeof val == 'object') {
+    // weakMap对象里，放的是键值对，key值必须是对象，不可以便利，使用set get方法设置和获取对应的值，
+    // 使用weakMap 是解决对象循环调用的情况，如obj.test = obj，如果不使用map，函数递归时会一直循环调用，进入死循环
+    // weakMap的好处时，键是弱引用，便于垃圾回收器回收
+    if (map.get(val)) return map.get(val) // 先判断key是否已有该对象，就是解决obj.test = obj，循环调用的情况
+    let target = new val.constructor() // 复制一个新对象，包含之前原型的引用
+    map.set(val, target) // 存储要处理的对象，便于以后查找
+    for(let key in val) { // 获取对象的key集合，包括对象和数组
+      if (val.hasOwnProperty(key)) { // 判断key是否对该对象自身的属性，不包括原型上的属性
+        target[key] = deepClone(val[key], map) // 这一步是关键，对象所有要赋值的数据都通过该函数处理，还要传递map，即整个递归过程，只生成一个weakmap对象
+      }
+    }
+    return target
+  } else {
+    return val
+  }
+}
--- src/pages/yuan/yuan.js
+++ src/pages/yuan/yuan.js
@@ -12,8 +12,8 @@ import '@/style/flexible.less'
 import '@/views/vue_extend/index.js'
 import '@/views/vue_extend1/toast.js'
 import {
-	Table,
-	TableColumn
+  Table,
+  TableColumn
 } from 'element-ui'
 
 Vue.use(Table)
@@ -22,75 +22,44 @@ Vue.use(TableColumn)
 const Vconsole = require('vconsole')
 const vConsole = new Vconsole()
 Vue.use(vConsole)
+
 // }
-function fa () {
-	return new Promise((resolve, reject) => {
-		setTimeout(() =>  {
-			// console.log('fn')
-			resolve(22)
-		}, 1000)
-	})
+function fa() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // console.log('fn')
+      resolve(22)
+    }, 1000)
+  })
 }
+
 let task = [fa]
 
-function fn1 (task) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() =>  {
-			task.forEach(item => {
-				resolve(item())
-			})
-		}, 1000)
-	})
+function fn1(task) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      task.forEach(item => {
+        resolve(item())
+      })
+    }, 1000)
+  })
 }
 
 
-async function ff () {
-	let a = await fn1(task)
-	// console.log(a)
+async function ff() {
+  let a = await fn1(task)
+  // console.log(a)
 }
+
 ff()
 
 Vue.config.productionTip = false
 // Vue.mixin(Mixin)
 new Vue({
-	router,
-	store,
-	render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
 
-let obj = {
-  name: 'yuan',
-  arr: [1, [2,3], 4],
-  fn: function () {
-    console.log('fn');
-  },
-  un: undefined,
-  nul: null,
-  reg: /boo/i
-}
-obj.test = obj
-
-// yuan.js?0fad:79 Uncaught RangeError: Maximum call stack size exceeded
-// 死循环
 
 
-function deepClone(val, hash = new WeakMap()) {
-  if (val instanceof Date) return new Date(val)
-  if (val instanceof RegExp) return new RegExp(val)
-  if (val === null) return null
-  if (typeof val != 'object') return val
-  if (hash.get(val)) return hash.get(val);
-  let target = new val.constructor()
-  hash.set(val, target);
-  for (let key in val) {
-    if (val.hasOwnProperty(key)) {
-      target[key] = deepClone(val[key])
-    }
-  }
-  return target
-}
-
-let obj1 = deepClone(obj)
-obj1.arr[1].push(4)
-obj1.arr[2] = 5
-console.log(obj, obj1, '123');
