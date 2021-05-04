c4dc1d0e443d30480d6ffa7ed56ad1f6e9d31353
13837120071 <xinyuan.ma@infaith.com.cn>
2021-05-04 23:15:49 +0800

    增加ts

--- typescript/helloWorld.js
+++ typescript/helloWorld.js
@@ -1,14 +1,17 @@
 /**
- * @interface 接口
+ * @interface 接口  https://mp.weixin.qq.com/s/KfOAu983zg8d0Uc-jhM84w
+ * interface与type的区别：一般来说，如果不清楚什么时候用interface/type，能用 interface 实现，就用 interface , 如果不能就用 type
  * */
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 function getUserInfo(user) {
     return "" + user.name + user.age;
 }
 console.log(getUserInfo({ name: 'ming', age: 18 }));
-function getUser(user) {
-    return "" + user.name + user.age;
-}
-console.log(getUser({ name: 'ming', age: 18 }));
 var user = {
     name: 'xing',
     age: 18
@@ -34,44 +37,214 @@ var fn = function (x) {
 fn(1);
 /**
  * 接口的实现（类implements实现接口）
+ * 类型规范，让一个类去实现某个接口，那么这个类就必须明确去拥有这个接口中的属性和实现其方法
  * */
-//类型规范，让一个类去实现某个接口，那么这个类就必须明确去拥有这个接口中的属性和实现其方法
 var Post = /** @class */ (function () {
-    function Post(title) {
+    function Post(title, name) {
         this.title = title;
+        this.name = name;
     }
     Post.prototype.log = function () {
         console.log(this.title);
     };
-    Post.prototype.getName = function () {
-        console.log(this.title);
-    };
     return Post;
 }());
-console.log(Post);
-var child = {
-    name: 'extends',
-    age: 18
+console.log(Post, 'post');
+var fnn = function (name) {
+    return name;
 };
-console.log(child);
-var son = {
-    name: 'tom',
-    score: "A",
-    height: 90
+console.log(fnn(1));
+console.log(fnn('123'));
+function identify(name) {
+    return name;
+}
+console.log(typeof identify(1), typeof identify(1));
+console.log(identify('str'));
+function change(name, age) {
+    return age;
+}
+console.log(change('1', 2));
+console.log(change(1, "2"));
+var enumArr = [1, '123'];
+console.log(enumArr);
+var fn1 = function (name) {
+    return name;
 };
-console.log(son, typeof son);
-var Children = /** @class */ (function () {
-    // constructor(name:string, score: string,height: number) {
-    function Children(name, score, height) {
-        this.name = name;
-        this.score = score;
-        this.height = height;
+var genericeFn = function (name) {
+    return name;
+};
+console.log(genericeFn('abcd'));
+console.log(genericeFn(1234));
+function getName(name) {
+    return name;
+}
+console.log(getName(666));
+var Gun = /** @class */ (function () {
+    function Gun(value) {
+        this.value = value;
     }
-    return Children;
+    Gun.prototype.getValue = function () {
+        return this.value;
+    };
+    return Gun;
 }());
-var child1 = new Children('child', 'A+', 99);
-console.log(child1, 'child1');
-var test;
-test = '123';
-test = 123;
-console.log(test);
+var gun1 = new Gun(1);
+var gun2 = new Gun("tttt");
+console.log(gun1, gun2);
+var GerMin = /** @class */ (function () {
+    function GerMin(name) {
+        this.name = name;
+    }
+    return GerMin;
+}());
+var germin = new GerMin(123);
+var germin1 = new GerMin('germin');
+console.log(germin, germin1);
+// 泛型函数
+function getAge(age) {
+    return age;
+}
+var gn = function (name) {
+    return name;
+};
+// 泛型类
+var Gan = /** @class */ (function () {
+    function Gan(name) {
+        this.name = name;
+    }
+    return Gan;
+}());
+var gan = new Gan('gan');
+console.log(gan);
+var gyn = {
+    name: '123',
+    age: '234'
+};
+console.log(gyn, 'gyn');
+var Gon = /** @class */ (function () {
+    function Gon(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Gon;
+}());
+var gon = new Gon('gon', '2');
+console.log(gon);
+function getLength(str) {
+    console.log(str.length);
+    return str;
+}
+function map(tuple) {
+    return [tuple[1], tuple[0]];
+}
+console.log(map([1, 'st']));
+function longIdentify(str) {
+    console.log(str.length);
+    return str;
+}
+console.log(longIdentify('123'));
+var defaultFn = {
+    name: '123'
+};
+console.log(defaultFn, '1234');
+var ina = function (name) {
+    return name;
+};
+console.log(ina('123'), 'ina');
+var generics = {
+    name: "str",
+    des: 'des'
+};
+console.log(generics, 'generics');
+var ClassT = /** @class */ (function () {
+    function ClassT(title) {
+        this.title = title;
+    }
+    return ClassT;
+}());
+var classT = new ClassT('123');
+console.log(classT, 'classT');
+/**
+ * class 类
+ * */
+var list = [1, 23, 4];
+var strList = ['a', 'b', 'c'];
+var tuple = ['123', 12]; // 元祖类型
+var numberList = [1, 3, 4];
+var stringList = ['1,3', '3', '34'];
+var Days;
+(function (Days) {
+    Days[Days["sun"] = 0] = "sun";
+    Days[Days["mon"] = 1] = "mon";
+    Days[Days["tue"] = 2] = "tue";
+    Days[Days["web"] = 3] = "web";
+    Days[Days["thu"] = 123] = "thu";
+    Days[Days["fri"] = 124] = "fri";
+})(Days || (Days = {})); // Days后面没有=， 也不需要用let const
+console.log(Days['sun'] === 0, Days[0] === 'sun'); // true true
+console.log(Days['mon'] === 1, Days[1] === 'mon'); // true true
+console.log(Days['thu'] === 123, Days[123] === 'thu'); // true true
+console.log(Days['fri'] === 124, Days[124] === 'fri'); // true true
+var Types;
+(function (Types) {
+    Types["sun"] = "3001";
+    Types["mon"] = "3002";
+    Types["tue"] = "3003";
+})(Types || (Types = {}));
+console.log(Types['sun'] === '3001'); // true
+/**
+ * @decorator 装饰器
+ *
+ * */
+function helloWorld(str) {
+    console.log('hello world');
+}
+var DecoratorClass = /** @class */ (function () {
+    function DecoratorClass() {
+    }
+    DecoratorClass = __decorate([
+        helloWorld
+    ], DecoratorClass);
+    return DecoratorClass;
+}());
+function addAge(age) {
+    return function (target) {
+        target.prototype.age = age;
+    };
+}
+var Hello = /** @class */ (function () {
+    function Hello() {
+        this.name = 'ming';
+    }
+    Hello = __decorate([
+        addAge(18)
+    ], Hello);
+    return Hello;
+}());
+var hello = new Hello();
+console.log(hello, 'hello');
+function addSex(sex) {
+    return function (target) {
+        target.prototype.sex = sex;
+    };
+}
+function addFn(sex) {
+    return function (target) {
+        target.prototype.sex = sex;
+        target.prototype.newFn = function () {
+            console.log(this.name);
+        };
+    };
+}
+var PersonFn = /** @class */ (function () {
+    function PersonFn() {
+        this.name = 'per';
+    }
+    PersonFn = __decorate([
+        addFn('男')
+    ], PersonFn);
+    return PersonFn;
+}());
+var pr = new PersonFn();
+console.log(pr);
+pr.newFn();
--- typescript/helloWorld.ts
+++ typescript/helloWorld.ts
@@ -2,21 +2,18 @@
  * @interface 接口  https://mp.weixin.qq.com/s/KfOAu983zg8d0Uc-jhM84w
  * interface与type的区别：一般来说，如果不清楚什么时候用interface/type，能用 interface 实现，就用 interface , 如果不能就用 type
  * */
+
 function getUserInfo (user: {name: string, age: number}):string {
   return `${user.name}${user.age}`
 }
 console.log(getUserInfo({name: 'ming', age: 18}));
 
+// 对象类型接口
 interface User{
   name: string;
   age: number;
   sex?: number; // 可选属性，对象是否有该属性都可以
 }
-function getUser(user: User):string {
-  return `${user.name}${user.age}`
-}
-console.log(getUser({name: 'ming', age: 18}));
-
 let user:User = {
   name: 'xing',
   age:18
@@ -46,6 +43,7 @@ console.log(per);
 console.log(per.getName('123'));
 per.getAge()
 
+// 函数类型接口
 interface Fuc {
   (x:number) :void
 }
@@ -59,24 +57,26 @@ interface Entify {
   log():void;
 }
 
+interface Inn {
+  name: string;
+}
 /**
  * 接口的实现（类implements实现接口）
+ * 类型规范，让一个类去实现某个接口，那么这个类就必须明确去拥有这个接口中的属性和实现其方法
  * */
-//类型规范，让一个类去实现某个接口，那么这个类就必须明确去拥有这个接口中的属性和实现其方法
-class Post implements Entify {
+class Post implements Entify, Inn {
   title: string;
-  constructor(title: string) {
+  name: string;
+  constructor(title: string,name:string) {
     this.title = title
+    this.name = name
   }
   log():void {
     console.log(this.title);
   }
-  getName():void {
-    console.log(this.title);
-  }
 }
 
-console.log(Post);
+console.log(Post, 'post');
 
 /**
  * 接口的继承 extends
@@ -85,44 +85,280 @@ console.log(Post);
 interface Parent {
   name: string;
 }
-interface Child extends Parent{
-  age: number
-}
-
-let child:Child = {
-  name: 'extends',
-  age: 18
-}
-console.log(child);
-
 interface Mother {
-  score: string;
+  color: string
+}
+interface Father {
+  height: number
+}
+interface Child extends Father, Mother{
+  age: number
 }
 
 interface Son extends Parent, Mother{
   height: number
 }
 
-let son:Son = {
-  name: 'tom',
-  score:"A",
-  height: 90
-}
-console.log(son, typeof son);
 
-class Children implements Son {
-  name:string;
-  score: string;
-  height: number;
-  // constructor(name:string, score: string,height: number) {
-  constructor(name:string, score: string,height: number) {
-    this.name = name
-    this.score = score
-    this.height = height
+
+/**
+ * 泛型 generics
+ * 泛型（Generics）是指在定义函数、接口或类的时候，不预先指定具体的类型，而在使用的时候再指定类型的一种特性。
+ * */
+interface In {
+  <T>(name:T):T
+}
+let fnn:In = function <T>(name:T):T {
+  return name
+}
+console.log(fnn<number>(1));
+console.log(fnn('123'));
+
+function identify <T>(name: T): T {
+  return name
+}
+
+console.log(typeof identify<Number>(1), typeof identify(1));
+console.log(identify('str'));
+
+function change<T, U>(name:T, age:U):U {
+  return age
+}
+
+console.log(change('1', 2));
+console.log(change(1, "2"));
+
+let enumArr: [number, string] = [1, '123']
+console.log(enumArr);
+
+interface Config {
+  <T>(value: T):T
+}
+
+// 函数类型接口
+interface Fnn {
+  (name:string): string
+}
+let fn1:Fnn = function (name: string):string {
+  return name
+}
+
+// 泛型接口
+interface GenericsFn {
+  <T>(name:T):T
+}
+let genericeFn:GenericsFn = function <T>(name:T):T {
+  return name
+}
+console.log(genericeFn('abcd'));
+console.log(genericeFn(1234));
+
+function getName <T>(name:T):T {
+  return name
+}
+
+console.log(getName(666));
+
+
+interface Gen<T> {
+  value:T,
+  getValue:() => T
+}
+class Gun<T> implements Gen<T> {
+  value:T
+  constructor(value:T) {
+    this.value = value
+  }
+  getValue():T {
+    return this.value
   }
 }
-let child1 = new Children('child', 'A+', 99)
-console.log(child1, 'child1');
+let gun1 = new Gun(1)
+let gun2 = new Gun("tttt")
+console.log(gun1,gun2);
+
+class GerMin<T> {
+  name: T
+  constructor(name:T) {
+    this.name = name
+  }
+}
+let germin = new GerMin<number>(123)
+let germin1 = new GerMin<string>('germin')
+console.log(germin, germin1);
+
+// 泛型函数
+function getAge <T>(age:T):T {
+  return age
+}
+// 泛型接口
+interface Gn {
+  <T>(name:T):T
+}
+let gn:Gn = function <T>(name:T):T {
+  return name
+}
+// 泛型类
+class Gan<T> {
+  name:T
+  constructor(name:T) {
+    this.name = name
+  }
+}
+let gan = new Gan<string>('gan')
+console.log(gan);
+
+interface Gyn<T> {
+  name:T;
+  age:T
+}
+let gyn:Gyn <string>= {
+  name: '123',
+  age: '234'
+}
+console.log(gyn, 'gyn');
+
+class Gon <T>implements Gyn<T>{
+  name:T
+  age:T
+  constructor(name:T, age:T) {
+    this.name = name
+    this.age = age
+  }
+}
+
+let gon = new Gon('gon', '2')
+console.log(gon);
+
+interface Length {
+  length: number
+}
+function getLength<T extends Length>(str:T):T {
+  console.log(str.length);
+  return str
+}
+
+function  map<T,U>(tuple:[T,U]):[U,T] {
+  return [tuple[1], tuple[0]]
+}
+
+console.log(map([1, 'st']));
+
+interface Lengthwise {
+  length:number
+}
+function longIdentify<T extends Lengthwise>(str:T):T {
+  console.log(str.length);
+  return str
+}
+
+console.log(longIdentify('123'));
+
+interface DefaultFn<T=string> {
+  name:T
+}
+
+let defaultFn:DefaultFn= {
+  name:'123'
+}
+console.log(defaultFn, '1234');
+
+interface Ina {
+  <T>(name:T):T
+}
+let ina:Ina = function <T>(name:T):T {
+  return name
+}
+console.log(ina<string>('123'), 'ina');
+interface GenericsObj<T>{
+  name:T;
+  des: T
+}
+let generics:GenericsObj<string> = {
+  name: "str",
+  des:'des'
+}
+console.log(generics, 'generics');
+class ClassT<T> {
+  title:T;
+  constructor(title:T) {
+    this.title = title
+  }
+}
+let classT = new ClassT<string>('123')
+console.log(classT, 'classT');
 
 
+/**
+ * class 类
+ * */
 
+let list: number[] = [1,23,4]
+let strList : string[] = ['a', 'b', 'c']
+let tuple: [string,number] = ['123',12] // 元祖类型
+
+let numberList:number[] = [1,3,4]
+let stringList :string[] = ['1,3', '3', '34']
+
+enum Days {sun, mon, tue, web, thu =123, fri} // Days后面没有=， 也不需要用let const
+console.log(Days['sun'] === 0, Days[0] === 'sun'); // true true
+console.log(Days['mon'] === 1, Days[1] === 'mon'); // true true
+console.log(Days['thu'] === 123, Days[123] === 'thu'); // true true
+console.log(Days['fri'] === 124, Days[124] === 'fri'); // true true
+
+enum Types {sun = '3001', mon='3002', tue = '3003'}
+console.log(Types['sun'] === '3001'); // true
+
+/**
+ * @decorator 装饰器
+ *
+ * */
+function  helloWorld(str:any):void {
+  console.log('hello world');
+}
+
+@helloWorld
+class DecoratorClass {
+}
+
+function addAge(age:number):Function{
+  return function (target:Function) {
+    target.prototype.age = age
+  }
+}
+
+@addAge(18)
+class Hello {
+  name:string;
+  constructor() {
+    this.name = 'ming'
+  }
+}
+let hello = new Hello()
+console.log(hello, 'hello');
+
+
+function addSex(sex:string):Function {
+  return function (target:Function) {
+    target.prototype.sex = sex
+  }
+}
+function addFn(sex:string):Function {
+  return function (target:Function) {
+    target.prototype.sex = sex
+    target.prototype.newFn = function () {
+      console.log(this.name);
+    }
+  }
+}
+
+@addFn('男')
+class PersonFn {
+  name:string;
+  constructor() {
+    this.name = 'per'
+  }
+}
+let pr = new PersonFn();
+console.log(pr);
+pr.newFn()
